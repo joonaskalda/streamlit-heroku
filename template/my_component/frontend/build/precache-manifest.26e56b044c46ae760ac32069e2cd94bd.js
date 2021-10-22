8c90b2c3ffee4ca33ef7d5d6f41c4bf24178400e
Joonas Kalda <joonas.kalda@gmail.com>
2021-10-22 13:58:24 +0300

    added build

---
+++ template/my_component/frontend/build/precache-manifest.26e56b044c46ae760ac32069e2cd94bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e35716efd81677915473e310e5094828",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f5127c440517d6e8ae79",
+    "url": "./static/js/2.62cee395.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.62cee395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eae49d70480dae3572a",
+    "url": "./static/js/main.c9406285.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  },
+  {
+    "revision": "cdc1a33845da1eb3f93ae5ac9ebdbdbe",
+    "url": "./static/media/3321821.cdc1a338.wav"
+  }
+]);
